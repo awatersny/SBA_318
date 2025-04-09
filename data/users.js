7339565b0f98a2840b26ae159b72f50ceb83e97b
awatersny <awatersny@gmail.com>
2025-04-09 14:45:25 -0400

    Set up user routing

---
+++ data/users.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    id: 2478,
+    userName: "Mobes",
+    species: "octoling",
+    favWeaponKit: "Dapple Dualies Nouveau",
+    favMap: "Museum d'Alfonsino",
+    img: ""
+  }
+]
+
+module.exports = users
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+const users = require("../data/users")
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(users)
+  })
+
+module.exports = router
