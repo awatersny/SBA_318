eb2749cc5431e1a0fe42311358488e0fab90bce3
awatersny <awatersny@gmail.com>
2025-04-14 21:15:50 -0400

    Fully populate stage data

--- data/stages.js
+++ data/stages.js
@@ -39,6 +39,86 @@ const stages = [
     name: "Museum d'Alfonsino",
     img: ""
   },
+  {
+    id: 9,
+    name: "Crableg Capital",
+    img: ""
+  },
+  {
+    id: 10,
+    name: "Inkblot Art Academy",
+    img: ""
+  },
+  {
+    id: 11,
+    name: "Eeltail Alley",
+    img: ""
+  },
+  {
+    id: 12,
+    name: "Manta Maria",
+    img: ""
+  },
+  {
+    id: 13,
+    name: "Hammerhead Bridge",
+    img: ""
+  },
+  {
+    id: 14,
+    name: "Sturgeon Shipyard",
+    img: ""
+  },
+  {
+    id: 15,
+    name: "Undertow Spillway",
+    img: ""
+  },
+  {
+    id: 16,
+    name: "Marlin Airport",
+    img: ""
+  },
+  {
+    id: 17,
+    name: "Shipshape Cargo Co.",
+    img: ""
+  },
+  {
+    id: 18,
+    name: "Mincemeat Metalworks",
+    img: ""
+  },
+  {
+    id: 19,
+    name: "Barnacle & Dime",
+    img: ""
+  },
+  {
+    id: 20,
+    name: "Bluefin Depot",
+    img: ""
+  },
+  {
+    id: 21,
+    name: "Mahi-Mahi Resort",
+    img: ""
+  },
+  {
+    id: 22,
+    name: "Wahoo World",
+    img: ""
+  },
+  {
+    id: 23,
+    name: "Robo ROM-en",
+    img: ""
+  },
+  {
+    id: 24,
+    name: "Lemuria Hub",
+    img: ""
+  }
 ]
 
 module.exports = stages
